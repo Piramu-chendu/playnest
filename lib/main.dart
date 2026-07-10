1a4f77d1165be04ea94e5e8e83fc01662c433f3d
Aswin <aswinramrm1061@gmail.com>
2026-07-10 14:49:45 +0530

    have added category page with 6 categories

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screens/splash/splash_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Make the status bar transparent for a more immersive feel
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
   runApp(const PlayNestApp());
 }
 
@@ -13,7 +22,15 @@ class PlayNestApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'PlayNest',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF09050F),
+        fontFamily: 'Roboto',
+        splashColor: Colors.white10,
+        highlightColor: Colors.white10,
+      ),
       home: const SplashScreen(),
     );
   }
 }
+
